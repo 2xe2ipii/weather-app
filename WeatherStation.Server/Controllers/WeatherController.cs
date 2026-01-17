f937495dded7a8541bfc2a936bd2cbd65271918c
Drexler Reyes <2xe2ipi@gmail.com>
2026-01-18 01:52:33 +0800

    removed unnecessary using directive from WeatherController.cs + added CORS policy to have React and .NET talk

--- WeatherStation.Server/Controllers/WeatherController.cs
+++ WeatherStation.Server/Controllers/WeatherController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using WeatherStation.Server;
 
 namespace WeatherStation.Server.Controllers;
 
--- WeatherStation.Server/Program.cs
+++ WeatherStation.Server/Program.cs
@@ -6,6 +6,22 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+// 1. Define the Policy Name
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// 2. Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                        policy.WithOrigins("http://localhost:5173")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();  
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
